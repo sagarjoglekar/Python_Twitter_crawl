7e57af2e7eddedbb900ab567649a4bcebeeb78f0
Sagar J <sagar.joglekar@citrix.com>
2015-01-11 01:18:24 -0800

    Have functioning app
    Use twitter API to do initial post on twitter as well as friend crawl

--- Oauthtwitter.py
+++ Oauthtwitter.py
@@ -2,6 +2,12 @@ import urlparse
 import oauth2 as oauth
 import ParseTwitterConfig
 import sys
+import twitter
+
+
+from time import gmtime, strftime
+
+
 
 config = ParseTwitterConfig.Parser(str(sys.argv[1]))
 config.parseConfig()
@@ -10,8 +16,20 @@ consumer_key = config.getConsumerKey()
 consumer_secret = config.getConsumerSecret()
 access_token = config.getAccessToken()
 access_secret = config.getAccessTokenSecret()
+owner = config.getOwner()
 
 api = twitter.Api(consumer_key=consumer_key,
                   consumer_secret=consumer_secret,
                   access_token_key=access_token,
                   access_token_secret=access_secret)
+
+print api.VerifyCredentials()
+
+statuses = api.GetUserTimeline(screen_name=owner)
+print [s.text for s in statuses]
+
+users = api.GetFriends()
+print [u.name for u in users]
+
+status = api.PostUpdate(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+print status.text
